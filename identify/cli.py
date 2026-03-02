a0e3bb1ea91f722f4eca2f6983cba12a14c37618
Andrew <andrew.saunder@biarrinetworks.com>
2026-03-03 00:19:28 +1100

    Opposite quotes

--- identify/cli.py
+++ identify/cli.py
@@ -11,7 +11,7 @@ from identify import identify
 def main(argv: Sequence[str] | None = None) -> int:
     parser = argparse.ArgumentParser(prog='identify')
     # https://stackoverflow.com/a/8521644/812183
-    parser.add_argument('-V', '--version', action='version', version=f'%(prog)s {importlib.metadata.version('identify')}')
+    parser.add_argument('-V', '--version', action='version', version=f'%(prog)s {importlib.metadata.version("identify")}')
     parser.add_argument(
         '--filename-only', action='store_true', help='Generate tags purely from the filename string passed in, avoiding any disk interaction.'
     )
